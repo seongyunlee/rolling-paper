767c83a9324e2cf65c3524aaffc13442791e7cac
seongyunlee <private.seongyun@gmail.com>
2023-02-17 19:08:12 +0900

    kakao

--- router/user.js
+++ router/user.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const controller = require("../controllers/user");
 
 // /paper/share/{id}
-router.post("/login", controller.login);
-router.get("/logout", controller.logout);
+//router.post("/login", controller.login);
+//router.get("/logout", controller.logout);
 //router.post("/register", controller.register);
 router.get("/list", controller.list);
 router.get("/koauth", controller.koauth);
