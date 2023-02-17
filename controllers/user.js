3c16beb1b09971c7650425c899e312aa557b4bb2
seongyunlee <private.seongyun@gmail.com>
2023-02-17 18:49:15 +0900

    kakao

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 const SALT_LENGTH = 10;
 const axios = require("axios");
 const configJson = require("../config.json");
-
+const JSON = require("js");
 // get post data body and redirect to referer
 exports.login = async (req, res) => {
   const { email, password } = req.body;
@@ -30,23 +30,19 @@ exports.logout = (req, res) => {
   }
 };
 // get post data body, make new user entitiy and redirect to referer
-const register = async (req, res) => {
+const register = async (kakao_uid) => {
   const { email, password } = req.body;
   const db = mongo.db("rollingpaper");
   const coll = db.collection("user");
-  const result = await coll.findOne({ id: email });
-  if (result) {
-    res.send(
-      "<script>alert('이미 존재하는 이메일입니다.');location.href='/register';</script>"
-    );
-  } else {
-    const salt = bcrypt.genSaltSync(SALT_LENGTH);
-    const hashed = bcrypt.hashSync(password, salt);
-    await coll.insertOne({ id: email, password: hashed });
-    res.send(
-      "<script>alert('회원가입 완료.');location.href='/login';</script>"
-    );
+  let newid = 0;
+  while (true) {
+    newid = Math.floor(Math.random() * 8999999999) + 1000000000;
+    const result = await coll.find({ id: newid }).toArray();
+    console.log(result);
+    break;
   }
+  await coll.insertOne({ uid: newid, kakao_uid: hashed });
+  return newid;
 };
 //make /list/{user_id} page
 exports.list = async (req, res) => {
@@ -85,24 +81,22 @@ exports.koauth = async (req, res) => {
   console.log(req.body?.id_token);
   console.log(req.query);
   const response = await getKakaoToken(req.query.code);
-  console.log(response);
-  res.send(
-    "<script>alert('비밀번호가 틀렸습니다.');location.href='/login';</script>"
+  const { header, payload, signature } = response.id_token;
+  const user_info = JSON.parse(
+    Buffer.from(payload, "base64").toString("utf-8")
   );
-  /*
-  const { email, password } = req.body;
+  console.log(user_info);
+
   const db = mongo.db("rollingpaper");
   const coll = db.collection("user");
-  const result = await coll.findOne({ id: email });
-  if (result && bcrypt.compareSync(password, result.password)) {
-    req.session.email = email;
-    req.session.save(() => {
-      res.redirect("/");
-    });
+  const result = await coll.findOne({ kakao_uid: user_info.sub });
+  if (result) {
+    const uid = result.uid;
   } else {
-    res.send(
-      "<script>alert('비밀번호가 틀렸습니다.');location.href='/login';</script>"
-    );
+    const uid = register(user_info.sub);
   }
-  */
+  req.session.uid = uid;
+  req.session.save(() => {
+    res.redirect("/");
+  });
 };
