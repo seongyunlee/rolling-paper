5b7caa95087288a47739966cd3ddbc8efc3606a5
seongyunlee <private.seongyun@gmail.com>
2023-02-17 12:37:37 +0900

    kakao

--- router/index.js
+++ router/index.js
@@ -6,7 +6,7 @@ const configJson = require("../config.json");
 router.get("/", controller.index);
 router.get("/login", (req, res) =>
   res.render("login", {
-    url: `https://kauth.kakao.com/oauth/authorize?client_id=${configJson.kakao_api}&redirect_uri=https://${configJson.domain}&response_type=code`,
+    url: `https://kauth.kakao.com/oauth/authorize?client_id=${configJson.kakao_api}&redirect_uri=https://${configJson.domain}/user/koauth&response_type=code`,
   })
 );
 router.get("/register", (req, res) => res.render("register", {}));
